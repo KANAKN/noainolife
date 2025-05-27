9e57a05548f09c0f3b93bb81820d6b884e8c6a83
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-05-28 00:17:45 +0900

    Reset to initial quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import Results from './components/Results';
-import { results } from './data/results';
+import Quiz from './components/Quiz';
 
 function App() {
-  // Get the Romanticist type result for testing
-  const romanticistResult = results.find(result => result.type === "ロマンチスト型");
-  
   return (
     <div className="min-h-screen bg-[#090034]">
-      <Results 
-        result={romanticistResult!}
-        totalScore={0}
-        onRestart={() => {}}
-      />
+      <Quiz />
     </div>
   );
 }
