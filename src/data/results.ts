761bc334b661ebbc0d1d22cd8e85d9617dea89ab
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 01:37:47 +0900

    Update Quiz component to show Romantic type result

--- src/data/results.ts
+++ src/data/results.ts
@@ -33,6 +33,12 @@ export const results: Result[] = [
         subtitle: "心も体もご機嫌！",
         items: [
           {
+            html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=250528019537&wid=001&eno=01&mid=s00000016987001073000&mc=1"></a><img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" alt="">'
+          },
+          {
+            html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+941V3M+5FC8+5ZEMP" rel="nofollow"><img border="0" width="250" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=250528019551&wid=001&eno=01&mid=s00000025316001005000&mc=1"></a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=455ONN+941V3M+5FC8+5ZEMP" alt="">'
+          },
+          {
             html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+941V3M+5FC8+5ZEMP" rel="nofollow"><img border="0" width="250" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=250528019551&wid=001&eno=01&mid=s00000025316001005000&mc=1"></a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=455ONN+941V3M+5FC8+5ZEMP" alt="">'
           },
           {
