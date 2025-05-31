f45fb4c0918e9fe8a9812f9b98330f629801b31d
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 01:53:00 +0900

    Update Results component spacing

--- src/data/results.ts
+++ src/data/results.ts
@@ -36,7 +36,7 @@ export const results: Result[] = [
             html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+95U5WY+5PIC+609HT" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=250528019554&wid=001&eno=01&mid=s00000026634001009000&mc=1"></a><img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=455ONN+95U5WY+5PIC+609HT" alt="">'
           },
           {
-            html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=250528019537&wid=001&eno=01&mid=s00000016987001073000&mc=1"></a><img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" alt="">'
+            html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" rel="nofollow"><img border="0"  alt="" src="https://www22.a8.net/svt/bgt?aid=250528019537&wid=001&eno=01&mid=s00000016987001073000&mc=1"></a><img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" alt="">'
           },
           {
             html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+941V3M+5FC8+5ZEMP" rel="nofollow"><img border="0" width="250" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=250528019551&wid=001&eno=01&mid=s00000025316001005000&mc=1"></a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=455ONN+941V3M+5FC8+5ZEMP" alt="">'
