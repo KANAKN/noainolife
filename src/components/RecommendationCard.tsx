a2924b0e4b80a3078f6b4c4782626bd77af83537
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 02:11:55 +0900

    Update Results component layout

--- src/components/RecommendationCard.tsx
+++ src/components/RecommendationCard.tsx
@@ -9,7 +9,7 @@ const RecommendationCard: React.FC<RecommendationCardProps> = ({ recommendation 
   if (recommendation.html) {
     return (
       <div 
-        className="w-45 h-full flex items-stretch"
+        className="w-42 h-full flex items-stretch"
         dangerouslySetInnerHTML={{ 
           __html: recommendation.html.replace(
             'margin:5px;',
