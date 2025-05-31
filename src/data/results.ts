2db3f82d34cf554192fc6ae362d49080fe6a0ec4
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 02:40:44 +0900

    Update Quiz component for Realist type test

--- src/data/results.ts
+++ src/data/results.ts
@@ -109,7 +109,13 @@ export const results: Result[] = [
           {
             html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+7K348I+4LSW+BXIYP" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www23.a8.net/svt/bgt?aid=250528019457&wid=001&eno=01&mid=s00000021488002004000&mc=1"></a><img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=455ONN+7K348I+4LSW+BXIYP" alt="">'
           },
-                    {
+          {
+            html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" rel="nofollow">
+<img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=250528019537&wid=001&eno=01&mid=s00000016987001073000&mc=1"></a>
+<img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" alt="">
+'
+          },
+          {
             html: '<a href="https://hb.afl.rakuten.co.jp/hsc/487c7368.5d5ac78a.487b6e56.3338ca58/?link_type=pict&rafst=rmn&ut=eyJwYWdlIjoic2hvcCIsInR5cGUiOiJwaWN0IiwiY29sIjoxLCJjYXQiOjEsImJhbiI6MjM3Nzg5NSwiYW1wIjpmYWxzZX0%3D" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"><img src="https://hbb.afl.rakuten.co.jp/hsb/487c7368.5d5ac78a.487b6e56.3338ca58/?me_id=2101065&me_adv_id=2377895&t=pict" border="0" style="margin:2px" alt="" title=""></a>'
           }
 
