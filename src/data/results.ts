e822fec6757c3b7fcd7bed18cfb644955bcc107b
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 01:40:51 +0900

    Updated results.ts

--- src/data/results.ts
+++ src/data/results.ts
@@ -35,7 +35,6 @@ export const results: Result[] = [
           {
             html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+95U5WY+5PIC+609HT" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=250528019554&wid=001&eno=01&mid=s00000026634001009000&mc=1"></a><img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=455ONN+95U5WY+5PIC+609HT" alt="">'
           },
-
           {
             html: '<a href="https://px.a8.net/svt/ejp?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=250528019537&wid=001&eno=01&mid=s00000016987001073000&mc=1"></a><img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=455ONN+8VPSMQ+3N2M+6DZBL" alt="">'
           },
