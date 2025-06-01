6fcd78cb071f6ff627643aa585828950ec08064f
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 16:35:16 +0900

    Fix dependency resolution error

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Quiz from './components/Quiz';
-import Results from './components/Results';
 import Footer from './components/Footer';
-import { results } from './data/results';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-[#090034] flex flex-col">
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Quiz />} />
-            <Route path="/result/:type" element={
-              <Results 
-                result={results[0]} 
-                totalScore={0} 
-                onRestart={() => window.location.href = '/'} 
-              />
-            } />
-          </Routes>
-        </div>
-        <Footer />
+    <div className="min-h-screen bg-[#090034] flex flex-col">
+      <div className="flex-grow">
+        <Quiz />
       </div>
-    </Router>
+      <Footer />
+    </div>
   );
 }
 
