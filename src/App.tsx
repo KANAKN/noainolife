c3b12ea5541e8071229c122090f6e9f256c05a18
KANAKN <73507751+KANAKN@users.noreply.github.com>
2025-06-01 16:29:06 +0900

    Set up dynamic routes and OGP meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Quiz from './components/Quiz';
+import Results from './components/Results';
 import Footer from './components/Footer';
+import { results } from './data/results';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#090034] flex flex-col">
-      <div className="flex-grow">
-        <Quiz />
+    <Router>
+      <div className="min-h-screen bg-[#090034] flex flex-col">
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Quiz />} />
+            <Route path="/result/:type" element={
+              <Results 
+                result={results[0]} 
+                totalScore={0} 
+                onRestart={() => window.location.href = '/'} 
+              />
+            } />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
